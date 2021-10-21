c4ac1c515aadd45a25209db4ced13d9ba955adc6
tejasksaindane <72094501+tejasksaindane@users.noreply.github.com>
2021-10-21 19:44:32 +0530

    commit

--- src/Components/dashpages/printDesign.jsx
+++ src/Components/dashpages/printDesign.jsx
@@ -6,101 +6,117 @@ import Layer4 from "../SGI/Layer20.png";
 import Layer5 from "../SGI/Layer21.png";
 import Layer6 from "../SGI/Layer22.png";
 import Layer7 from "../SGI/Layer23.png";
-// import Lotties from "react-lottie";
 
 function printDesign() {
   return (
     <>
       <div className="dashpage">
-        {/* <video autoplay muted loop id="myVideo">
-          <source src="rain.mp4" type="video/mp4" />
-        </video> */}
+        <div className="left animate__animated animate__fadeInUp animate__slow">
+          {/* <h1>hello</h1> */}
 
-        <div className="left">
-          <h1 className="animate__animated animate__fadeInUp animate__slow left_h1">
-            Print Design
-          </h1>
-          <div className="left-img animate__animated animate__fadeInUp animate__slow">
-            <img className="left-img-one gap" src={Layer6} alt="" />
-            <img className="left-img-one gap" src={Layer4} alt="" />
-            <img className="left-img-one gap" src={Layer1} alt="" />
-            <img className="left-img-one gap" src={Layer2} alt="" />
-          </div>
+          <center>
+            <h1>Print Design</h1>
+            <br />
+            <img className="dashpage__image" src={Layer1}></img>
+            <img className="dashpage__image" src={Layer2}></img>
+            <img className="dashpage__image" src={Layer3}></img>
+            <img className="dashpage__image" src={Layer4}></img>
+          </center>
         </div>
-        <div className="right">
-          {/* <div className="column1">
-            <img className="shadow" src={Layer1} alt="" />
-            <img src={Layer2} alt="" />
-            <img className="shadow" src={Layer3} alt="" />
-          </div>
-          <div className="column2">
-            <img className="shadow" src={Layer4} alt="" />
-            // <img src={Layer5} alt="" />
-            <img src={Layer6} alt="" />
-            <img className="shadow" src={Layer7} alt="" /> */}
-          {/* </div> */}
-          <div className="right-img">{/* <img src={Layer5} alt="" /> */}</div>
+        <div className="right-One animate__animated animate__fadeInUp animate__slow">
+          <center>
+            {/* <h3 className="dashpage_h3">
+              New <br />
+              Summer Collection
+            </h3> */}
+            {/* <p className="right-One-text">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam
+              soluta blanditiis placeat, ipsa quibusdam quaerat non Lorem ipsum
+              dolor sit amet consectetur
+            </p> */}
+          </center>
         </div>
       </div>
       <style jsx>{`
-        @import url("https://fonts.googleapis.com/css2?family=Rampart+One&display=swap");
-        .left_h1 {
+        .left {
+          font-family: "Rampart One", cursive;
+          // background-color: white;
+        }
+        .dashpage__image {
+          width: 200px;
+          height: 250px;
+          padding-bottom: 25px;
+          margin: 8px;
+        }
+        .right-One {
+          // display: flex;
+          // flex-wrap: wrap;
           font-family: "Rampart One", cursive;
         }
-        .shadow {
-          padding-top: 30px;
+
+        .dashpage_h3 {
+          color: #fee362;
         }
-        .dashpage-text {
-          padding-top: 20px;
-          font-size: 32px;
-        }
-        .dashpage {
-          display: flex;
-          flex-direction: row;
-          box-sizing: border-box;
-          overflow: hidden;
-        }
-        .gap{
-         padding:5px;
-        }
-        .dashpage .left h1 {
-          margin: 0;
-          padding: 20px;
-          font-size: 2.2rem;
-          color: #fff;
-          font-style: italic;
-          text-transform: uppercase;
-        }
-        .left {
-          width: 40%;
-          padding: 20px;
-          overflow: hidden;
-        }
-        hr {
-          background-color: #fff;
-        }
-        .left table {
-          width: 100%;
-        }
-        .left td {
-        }
-        .left p {
-          font-size: 16px;
-        }
-        .left-img {
-          display: flex;
-          align-items: center;
-          justify-content: space-around;
-          flex-wrap: wrap;
-          padding:12px;
-        }
-        .left-img-one{
-          width:110px; !important; 
-          height:110px; !important;
+        .right-One-text {
+          margin-bottom: 30px !important;
         }
 
-        .right {
-        }
+        @import url("https://fonts.googleapis.com/css2?family=Rampart+One&display=swap");
+        // .left_h1 {
+        //   font-family: "Rampart One", cursive;
+        // }
+        // .shadow {
+        //   padding-top: 30px;
+        // }
+        // .dashpage-text {
+        //   padding-top: 20px;
+        //   font-size: 32px;
+        // }
+        // .gap{
+        //  padding:5px;
+        // }
+        // .dashpage .left h1 {
+        //   margin: 0;
+        //   padding: 20px;
+        //   font-size: 2.2rem;
+        //   color: #fff;
+        //   font-style: italic;
+        //   text-transform: uppercase;
+        // }
+        // .left {
+        //   width: 40%;
+        //   padding: 20px;
+        //   overflow: hidden;
+        // }
+        // hr {
+        //   background-color: #fff;
+        // }
+        // .left table {
+        //   width: 100%;
+        // }
+        // .left td {
+        // }
+        // .left p {
+        //   font-size: 16px;
+        // }
+        // .left-img {
+        //   display: flex;
+        //   align-items: center;
+        //   justify-content: space-around;
+        //   flex-wrap: wrap;
+        //   padding:12px;
+        // }
+        // .left-img-one{
+        //   width:110px; !important;
+        //   height:110px; !important;
+        // }
+
+        // .right {
+        //   width: 40%;
+        //   padding: 20px;
+        //   box-sizing:border-box;
+        //   overflow: hidden;
+        // }
         //
         // .column1,
         // .column2 {
@@ -115,20 +131,39 @@ function printDesign() {
         //   margin-top: -100px;
         //   clip-path: inset(80px 0 110px 0);
         // }
-        .left-img {
-          display: flex;
-          align-items: center;
-          justify-content: space-around;
-          flex-wrap: wrap;
-          // padding:10px;
+        // .left-img {
+        //   display: flex;
+        //   align-items: center;
+        //   justify-content: space-around;
+        //   flex-wrap: wrap;
+        //   padding:10px;
+        // }
+        .right-img {
+          // width: 450px;
+          // height: 400px;
+          // padding-top: 50px;
+          // padding-left: 70px;
+          // overflow: hidden;
+          // display: flex;
+          // align-items: center;
+          // justify-content: space-around;
+          // flex-wrap: wrap;
         }
-        .right img {
-          width: 450px;
-          height: 400px;
-          padding-top: 50px;
-          padding-left: 70px;
-          overflow: hidden;
+        @media screen and (max-width: 1000px) {
+          .dashpage__image {
+            width: 140px;
+            height: 180px;
+            // padding-bottom: 40px;
+          }
         }
+        @media screen and (max-width: 1250px) {
+          .dashpage__image {
+            width: 140px;
+            height: 180px;
+            // padding-bottom: 40px;
+          }
+        }
+
         @media screen and (max-width: 800px) {
           .left {
             width: 100%;
@@ -140,31 +175,41 @@ function printDesign() {
             display: flex;
             flex-direction: column;
           }
-          .column1 {
-            margin-top: -20px;
-            clip-path: inset(20px 0 154px 0);
-          }
-          .column2 {
-            margin-top: -80px;
-            clip-path: inset(80px 0 154px 0);
-          }
-        }
-        @media screen and (max-width: 500px) {
-          .right img {
-            width: 50px;
-            // padding: 15px 10px;
+
+          .right-One-text {
+            margin: 30px;
             overflow: hidden;
           }
-          .column1 {
-            margin-top: 10px;
-            clip-path: inset(0 0 140px 0);
+          // .column1 {
+          //   margin-top: -20px;
+          //   clip-path: inset(20px 0 154px 0);
+          // }
+          // .column2 {
+          //   margin-top: -80px;
+          //   clip-path: inset(80px 0 154px 0);
+          // }
+        }
+        @media screen and (max-width: 500px) {
+          // .right img {
+          //   width: 50px;
+          //   // padding: 15px 10px;
+          //   overflow: hidden;
+          // }
+          .dashpage__image {
+            width: 120px;
+            height: 120px;
+            padding-bottom: 40px;
           }
-          .column2 {
-            clip-path: inset(100px 0 151px 0);
-          }
-          .left {
-            padding: 5px;
-          }
+          // .column1 {
+          //   margin-top: 10px;
+          //   clip-path: inset(0 0 140px 0);
+          // }
+          // .column2 {
+          //   clip-path: inset(100px 0 151px 0);
+          // }
+          // .left {
+          //   padding: 5px;
+          // }
         }
       `}</style>
     </>
